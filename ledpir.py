233cdca81a1dfb3b72cec12dcef29fcc11a83e0c
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-12-01 19:44:13 +0000

    led on xnay

--- ledpir.py
+++ ledpir.py
@@ -2,9 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM) #Set GPIO to pin numbering
+GPIO.setwarnings(False)
 pir = 23 #Assign pin 8 to PIR
 led = 21 #Assign pin 10 to LED
 tled = 17
+buz= 13
+buzzState = False
+GPIO.setup(buz, GPIO.OUT)
 GPIO.setup(pir, GPIO.IN) #Setup GPIO pin PIR as input
 GPIO.setup(led, GPIO.OUT) #Setup GPIO pin for LED as output
 GPIO.setup(tled, GPIO.OUT)
@@ -25,11 +29,15 @@ try:
       GPIO.output(led, GPIO.LOW) #Turn off LED
       time.sleep(.5)
       #some chanfes
+      '''while True:
+        buzzState = not buzzState
+        GPIO.output(buz, buzzState)
+        time.sleep(1)'''
     else:
       print ("No Motion Detected!")
       GPIO.output(tled, GPIO.HIGH) #Turn on LED
       time.sleep(4) #Keep LED on for 4 seconds
-      print ("led on!")
+      #print ("led on!")
       GPIO.output(tled, GPIO.LOW) #Turn off LED
       time.sleep(.5)
       #some chanfes
