5ffe255cec853db4f4a78e617cf285ecb818f1cb
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-12-08 20:27:33 +0000

    gyiig

--- temp.py
+++ temp.py
@@ -1,13 +1,31 @@
 import RPi.GPIO as GPIO
 import time
+import board
+import adafruit_dht
+import psutil
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-temp= 12
-buzzState = False
-GPIO.setup(temp, GPIO.IN)
+
+
+# We first check if a libgpiod process is running. If yes, we kill it!
+for proc in psutil.process_iter():
+    if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+        proc.kill()
+
+sensor = adafruit_dht.DHT11(board.D12)
 
 while True:
-    buzzState = not buzzState
-    print(GPIO.input(temp))
-    time.sleep(2)
+    try:
+        temp = sensor.temperature
+        humidity = sensor.humidity
+        print("Temperature: {}*C   Humidity: {}% ".format(temp, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        sensor.exit()
+        raise error
+
+    time.sleep(2.0)
